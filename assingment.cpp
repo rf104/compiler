5c09a93b3655acf2ccdd9e38583a381ceef64c7a
rf_104 <kingaref13@gmail.com>
2024-05-09 19:53:49 +0600

    !!

--- assingment.cpp
+++ assingment.cpp
@@ -1,22 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+int a[10];
+
+int Hash(string sym)
 {
-    int t;
-    cin>>t;
-    while(t--)
+    int l = sym.length();
+    int t = (l*202214056) % 10;
+    a[t]++;
+    return t;
+}
+
+class Symbolinfo{
+    string name ,symbol_type;
+    public:
+    Symbolinfo()
     {
-        int a,b;
-        cin>>a>>b;
-        if(a==1)
+        this->name = "";
+        this->symbol_type ="";
+    }
+    Symbolinfo(string a, string b)
+    {
+        this->name = a;
+        this->symbol_type =  b;
+    }
+    string getName()
+    {
+        return this->name;
+    }
+    string getType()
+    {
+        return this->symbol_type;
+    }
+    void display()
+    {
+        int y = Hash(this->name);
+        cout<<"<"<<this->name<<","<<this->symbol_type<<">";
+    }
+};
+
+class SymbolTable{
+    map<int,vector<Symbolinfo>>ht;
+    public:
+    void print()
+    {
+        for (auto it = ht.begin(); it!= ht.end(); it++)
         {
-            //int k = b/2;
-            if(b%2==1) cout<<b+1<<"\n";
-            else cout<<b/2<<"\n";
-        }
-        else
-        {
-            cout<<b/a+1<<"\n";
+            cout<<it->first<<"\n";
         }
     }
+    void insrt(Symbolinfo obj1)
+    {
+        int y = Hash(obj1.getName());
+        ht[y].push_back(obj1);
+        cout<<"Inserted into Hash Table!\n";
+    }
+};
+
+
+int main()
+{
+    
 }
